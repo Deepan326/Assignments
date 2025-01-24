913f8307fdb4ea0b5ee780e79b7038ed44acf320
Deepan <rdeepan326@gmail.com>
2025-01-24 22:22:18 +0530

    Assignment 12

---
+++ assignment12/script.js
@@ -0,0 +1,14 @@
+
+let stones = +(prompt("Enter the number of stones:"));
+
+    if (stones % 4 === 0) {
+        document.write("Your friend won the game.");
+    } else {
+        document.write("You won the game.");
+    }
+
+
+       
+   
+
+
